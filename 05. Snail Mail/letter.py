08f3eaa634e5c55ca9906e46d25feb2fcd3a5627
Inf015 <inf-15@olivers-MacBook-Pro.local>
2025-07-13 22:29:04 -0400

    adding 05 Snail Mail of Codedex

---
+++ 05. Snail Mail/letter.py
@@ -0,0 +1,16 @@
+# Write code below 💖
+
+# I am a race driver
+
+print("OOOOO   IIIII")
+print("O   O     I  ")
+print("O   O     I  ")
+print("O   O     I  ")
+print("O   O     I  ")
+print("OOOOO   IIIII")
+print('')
+print('07/13/2025')
+print('Im feel soo confuse')
+print('Get a nice job too make proud my familie')
+print('Keep going your magic is never give up')
+print('🏎️')
