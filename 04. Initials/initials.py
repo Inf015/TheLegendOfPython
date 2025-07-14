2fea8bd62698485e0ef5d76a86900e4cc49e4a1b
Inf015 <inf-15@olivers-MacBook-Pro.local>
2025-07-13 22:03:09 -0400

    adding 04 pattern of Codedex

---
+++ 04. Initials/initials.py
@@ -0,0 +1,10 @@
+# Write code below 💖
+
+# I am a race driver
+
+print("OOOOO   IIIII")
+print("O   O     I  ")
+print("O   O     I  ")
+print("O   O     I  ")
+print("O   O     I  ")
+print("OOOOO   IIIII")
